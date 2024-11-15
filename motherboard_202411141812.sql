439abd98f14082bac5f32f29fc5fb8533213ce2f
MasonElizondo <143554418+MasonElizondo@users.noreply.github.com>
2024-11-14 18:13:55 -0600

    Add files via upload
    these one work

---
+++ motherboard_202411141812.sql
@@ -0,0 +1,11 @@
+INSERT INTO pixel.motherboard (Motherboard_id,socket_type,wifi,bluetooth) VALUES
+	 (41,'LGA1700',1,0),
+	 (42,'LGA1700',1,0),
+	 (43,'AM4',1,1),
+	 (44,'LGA1700',1,0),
+	 (45,'AM4',0,1),
+	 (46,'AM4',0,1),
+	 (47,'AM5',0,0),
+	 (48,'AM4',1,0),
+	 (49,'AM4',0,0),
+	 (50,'LGA1700',1,1);
